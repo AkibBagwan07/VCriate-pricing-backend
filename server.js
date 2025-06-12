adb04c2b15acb4f0bdb92616f5be40cd8d66f2f3
Akib Bagwan <akibbagwan69@gmail.com>
2025-06-12 23:57:50 +0530

    file name typo fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const pricingRoutes = require('./routes/pricing');
+const pricingRoutes = require('./routes/Pricing');
 require('dotenv').config();
 
 const app = express();
